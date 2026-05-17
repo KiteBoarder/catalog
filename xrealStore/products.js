5b1ec1019c17f3ea5e9e266498b6f1479eb32c77
KiteBoarder <athc2006@gmail.com>
2026-05-17 00:25:46 -0500

    Add xrealStore: products.js

---
+++ xrealStore/products.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    name: "Product 1",
+    description: "Coming soon.",
+    image: "images/placeholder.svg",
+    amazonUrl: "#",
+    badge: ""
+  },
+  {
+    name: "Product 2",
+    description: "Coming soon.",
+    image: "images/placeholder.svg",
+    amazonUrl: "#",
+    badge: ""
+  }
+];
